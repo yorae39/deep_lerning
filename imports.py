e4d75e8a2feaaf4cf984770a8aa38294dd4b0203
Luiz Paulo <lpaureliano74@gmail.com>
2019-05-15 00:08:06 -0300

    imports de ferramentas

---
+++ imports.py
@@ -0,0 +1,19 @@
+"AULA INICIAL DE DEEP LEARNING - "
+
+import numpy as np
+
+a = 45
+b = 42 
+
+np.max([a, b])
+
+
+import sys
+sys.version
+
+import tensorflow
+tensorflow.VERSION
+
+
+import keras
+keras.__version__
